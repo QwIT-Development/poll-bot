bf0f110a05da3f592798f28daf354ce44a0d4006
Pearoo <63553775+rnxm@users.noreply.github.com>
2025-08-28 01:34:01 +0200

    feat: update message when poll ends

--- log.py
+++ log.py
@@ -20,7 +20,7 @@ class PollLog(commands.Cog):
 		options = []
 		for i, item in enumerate(poll.answers, start=1):
 			emoji = str(item.emoji) if item.emoji else f"{i}\uFE0F\u20E3"
-			percentage = round(item.vote_count / poll.total_votes * 100, 2) if poll.total_votes > 0 else 0
+			percentage = round(item.vote_count / poll.total_votes * 100) if poll.total_votes > 0 else 0
 			options.append(f"{emoji} {item.text} `{percentage}%`")
 
 		multiple = "multiple options" if poll.multiple else "only one option"
@@ -35,14 +35,6 @@ class PollLog(commands.Cog):
 		)
 		return message
 
-	def update_poll(self, poll: discord.Poll) -> discord.Message | None:
-		message = self.polls.get(poll.message.id)
-		print(message, poll.message.id, self.polls.keys())
-		if message is None:
-			return None
-		self.polls.update({poll.message.id: poll.message})
-		return message
-
 	@commands.Cog.listener()
 	async def on_message(self, message: discord.Message):
 		if message.poll:
@@ -52,15 +44,33 @@ class PollLog(commands.Cog):
 
 	@commands.Cog.listener()
 	async def on_poll_vote_add(self, user: Union[discord.User, discord.Member], answer: discord.PollAnswer):
-		message = self.update_poll(answer.poll)
+		message = self.polls.get(answer.poll.message.id)
 		if message is not None:
 			await message.edit(content=self.create_poll_log_message(answer.poll.message) + f"\n\n{user.mention} voted for \"{answer.text}\"")
 
 	@commands.Cog.listener()
 	async def on_poll_vote_remove(self, user: Union[discord.User, discord.Member], answer: discord.PollAnswer):
-		message = self.update_poll(answer.poll)
+		message = self.polls.get(answer.poll.message.id)
 		if message is not None:
 			await message.edit(content=self.create_poll_log_message(answer.poll.message) + f"\n\n{user.mention} removed their vote from \"{answer.text}\"")
+
+	@commands.Cog.listener()
+	async def on_message_edit(self, before: discord.Message, after: discord.Message):
+		message = after
+		if message.author.bot:
+			return
+		if message.poll and message.poll.is_finalised():
+			poll_message = self.polls.get(message.id)
+			if poll_message is not None:
+				victor_answer = message.poll.victor_answer or max(message.poll.answers, key=lambda x: x.vote_count)
+				poll_message_content = self.create_poll_log_message(message)
+				poll_ended = (
+					f"The poll has closed" + (" early" if message.poll.expires_at < datetime.datetime.now(tz=datetime.timezone.utc) else "") + "."
+					f"\n## Winner"
+					f"\n**{victor_answer.text}** `({round(victor_answer.vote_count / (message.poll.total_votes or 1) * 100)}%)`"
+				)
+				await poll_message.edit(content=poll_message_content + f"\n\n{poll_ended}")
+				self.polls.pop(message.id)
 
 async def setup(bot: commands.Bot):
 	await bot.add_cog(PollLog(bot, await bot.fetch_channel(1410278509569507420)))
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from os import getenv
 import sys
 import logging
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 logger = logging.getLogger(__name__)
 
